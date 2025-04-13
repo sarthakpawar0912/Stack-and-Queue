cab142d32a2f48664206b1232bd98684390eeb41
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-13 18:07:44 +0530

    Fixed bugs and updated code

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -5,35 +5,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-//        Stack<Integer> stack=new Stack<>();
-//        stack.push(32);
-//        stack.push(4);
-//        stack.push(7);
-//        stack.push(82);
-//        stack.push(13);
-//        stack.push(55);
-//
-//        System.out.println(stack.pop());
-//
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
+        Stack<Integer> stack=new Stack<>();
+        stack.push(32);
+        stack.push(4);
+        stack.push(7);
+        stack.push(82);
+        stack.push(13);
+        stack.push(55);
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
 
 
 
 
 
-//        //Queue
-//        Queue<Integer>queue=new LinkedList<>();
-//        queue.add(3);
-//        queue.add(6);
-//        queue.add(34);
-//        queue.add(98);
-//        queue.add(67);
-//        queue.add(41);
-//        queue.add(56);
-//
-//        System.out.println(queue.remove());
-//        System.out.println(queue.remove());
+        //Queue
+        Queue<Integer>queue=new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(34);
+        queue.add(98);
+        queue.add(67);
+        queue.add(41);
+        queue.add(56);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
 
 
         Deque<Integer>deque=new ArrayDeque<>();
