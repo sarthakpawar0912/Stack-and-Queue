e32a870f37f28df7f01c56f124dc4b37bb33416b
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-11 22:26:58 +0530

    Fixed bugs and updated code

--- Stack/src/CDAC/Concept/Queue.java
+++ Stack/src/CDAC/Concept/Queue.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 // Class to implement a linear queue
 class LinearQueue {
+
     private int[] arr; // Array to store queue elements
     private int front; // Index of front element
     private int rear; // Index of rear element
