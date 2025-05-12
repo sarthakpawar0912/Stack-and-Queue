4df50107cd4ed486054c5f47a78fc985cbf61b7f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-13 00:10:08 +0530

    Fixed bugs and updated code

--- Stack/src/CDAC/Concept/Stackmain.java
+++ Stack/src/CDAC/Concept/Stackmain.java
@@ -162,7 +162,7 @@ public class Stackmain {
 
                     break;
 
-                    case 3:
+                case 3:
                     System.out.println("The value at the top of the stack is: " + s.peek());
                     s.display(); // Display stack after peek
                     break;
